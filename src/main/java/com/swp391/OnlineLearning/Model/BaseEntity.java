de0351ec4760b427a83aa5c861446f5cf64aaa25
Huy <hn662945@gmail.com>
2026-04-15 13:25:54 +0700

    make BaseEntity abstract

--- src/main/java/com/swp391/OnlineLearning/Model/BaseEntity.java
+++ src/main/java/com/swp391/OnlineLearning/Model/BaseEntity.java
@@ -3,6 +3,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-public class BaseEntity {
+import java.time.LocalDateTime;
+@MappedSuperclass
+public abstract class BaseEntity {
 
 }
