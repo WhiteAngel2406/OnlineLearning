62aa24229d07650c49dca1c5ed55b852f195d532
Huy <hn662945@gmail.com>
2026-04-15 13:24:35 +0700

    create BaseEntity

---
+++ src/main/java/com/swp391/OnlineLearning/Model/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.swp391.OnlineLearning.Model;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+public class BaseEntity {
+
+}
