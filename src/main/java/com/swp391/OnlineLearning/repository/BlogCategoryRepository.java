3b318486d081a7380cf220be873a0d9c30392a65
Hoàng Minh Bách <153330033+hoangminhbach@users.noreply.github.com>
2026-04-19 19:21:32 +0700

    Remove OnlineLearning Java backend files

--- src/main/java/com/swp391/OnlineLearning/repository/BlogCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swp391.OnlineLearning.repository;
-
-import com.swp391.OnlineLearning.model.BlogCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogCategoryRepository extends JpaRepository<BlogCategory, Long> {
-}
--- src/main/java/com/swp391/OnlineLearning/service/BlogCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.swp391.OnlineLearning.service;
-
-import com.swp391.OnlineLearning.model.BlogCategory;
-
-import java.util.List;
-
-public interface BlogCategoryService {
-    List<BlogCategory> findAll();
-}
